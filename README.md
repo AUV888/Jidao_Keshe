59eecb8e28ddd332858503556a95e86f95ce9730
AUV888 <bigsticks114514@gmail.com>
2025-03-13 17:36:17 +0800

    在代码中加入了删除节点的函数

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-**This is the homework and the references of the course "Ji_Suan_Dao_Lun_Yu_Cheng_Xu_She_Ji_Ke_Cheng_She_Ji".**
+**This repository includes the homeworks and the references of the course "Ji_Suan_Dao_Lun_Yu_Cheng_Xu_She_Ji_Ke_Cheng_She_Ji".**
 **这个仓库包括了大学第2学期课程《计算导论与程序设计——课程设计》中资料和我的代码**
--- lesson_01/node.c
+++ lesson_01/node.c
@@ -1,102 +1,131 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Enter -1 to end input
+// Enter -1 to end input
 struct node
 {
     int data;
-    struct node* next;
+    struct node *next;
 };
 typedef struct node NODE;
-typedef struct node* NODEPTR;
+typedef struct node *NODEPTR;
 
 NODEPTR create_node(void);
-void insert_node(NODEPTR previous,NODEPTR *head);//Send the address of the pointer head
+void insert_node(NODEPTR previous, NODEPTR *head); // Send the address of the pointer head
 void output_node_list(NODEPTR head);
-NODEPTR get_node(int n,NODEPTR head);//Counting from 0,1...
+void delete_node(NODEPTR previous, NODEPTR *head);
+NODEPTR get_node(int n, NODEPTR head); // Counting from 0,1...
 
 int main(void)
 {
     NODEPTR hd;
-    hd=create_node();
+    hd = create_node();
+    printf("original node list:\n");
     output_node_list(hd);
-    printf("---\n%d",get_node(2,hd)->data);
+    // printf("---\n%d\n",get_node(2,hd)->data);
+    printf("\ninsert at no.:");
+    int tmp;
+    scanf("%d", &tmp);
+    insert_node(get_node(tmp - 1, hd), &hd);
+    printf("\n-----after insert\n");
+    output_node_list(hd);
+    printf("\ndelete no.:");
+    scanf("%d", &tmp);
+    delete_node(get_node(tmp - 1, hd), &hd);
+    printf("\n-----after delete\n");
+    output_node_list(hd);
     free(hd);
     return 0;
 }
 
 NODEPTR create_node(void)
 {
-    NODEPTR currentptr=NULL,headptr=NULL,lastptr=NULL;
+    NODEPTR currentptr = NULL, headptr = NULL, lastptr = NULL;
     int num;
-    scanf("%d",&num);
-    while(num!=-1)
+    scanf("%d", &num);
+    while (num != -1)
     {
-        currentptr=malloc(sizeof(NODE));
-        currentptr->data=num;
-        if(headptr==NULL)//It is the head of node
+        currentptr = malloc(sizeof(NODE));
+        currentptr->data = num;
+        if (headptr == NULL) // It is the head of node
         {
-            headptr=currentptr;
-            lastptr=currentptr;
-            currentptr->next=NULL;
+            headptr = currentptr;
+            lastptr = currentptr;
+            currentptr->next = NULL;
         }
-        else//In the middle of node
+        else // In the middle of node
         {
-            lastptr->next=currentptr;
-            lastptr=currentptr;
-            currentptr->next=NULL;
+            lastptr->next = currentptr;
+            lastptr = currentptr;
+            currentptr->next = NULL;
         }
-        scanf("%d",&num);
+        scanf("%d", &num);
     }
     return headptr;
 }
 
-void insert_node(NODEPTR previous,NODEPTR *head)//Send the address of the pointer head
+void insert_node(NODEPTR previous, NODEPTR *head) // Send the address of the pointer head
 {
-    NODEPTR newptr=malloc(sizeof(NODE));
-    scanf("%d",&newptr->data);
-    if(previous==NULL)//Insert the node at the first place
+    NODEPTR newptr = malloc(sizeof(NODE));
+    scanf("%d", &newptr->data);
+    if (previous == NULL) // Insert the node at the first place
     {
-        NODEPTR current=*head;
-        *head=newptr;
-        newptr->next=current;
+        NODEPTR current = *head;
+        *head = newptr;
+        newptr->next = current;
     }
-    else if(previous->next==NULL)//Insert the node at the last place
+    else if (previous->next == NULL) // Insert the node at the last place
     {
-        previous->next=newptr;
-        newptr->next=NULL;
+        previous->next = newptr;
+        newptr->next = NULL;
     }
-    else //In the middle of node
+    else // In the middle of node
     {
-        NODEPTR current=previous->next;
-        previous->next=newptr;
-        newptr->next=current;
+        NODEPTR current = previous->next;
+        previous->next = newptr;
+        newptr->next = current;
     }
     return;
 }
 void output_node_list(NODEPTR head)
 {
-    if(head==NULL)
+    if (head == NULL)
     {
         printf("Empty Node List\n");
         return;
     }
-    NODEPTR current=head;
-    while(current!=NULL)
+    NODEPTR current = head;
+    while (current != NULL)
     {
-        printf("%d\n",current->data);
-        current=current->next;
+        printf("%d\t", current->data);
+        current = current->next;
     }
     return;
 }
-NODEPTR get_node(int n,NODEPTR head)//counting from 0,1...
+NODEPTR get_node(int n, NODEPTR head) // counting from 0,1...
 {
-    int cnt=0;
-    NODEPTR current=head;
-    while(cnt<n)
+    int cnt = 0;
+    NODEPTR current = head;
+    while (cnt < n)
     {
-        current=current->next;
+        current = current->next;
         cnt++;
     }
     return current;
+}
+void delete_node(NODEPTR previous, NODEPTR *head)
+{
+    if (previous == NULL) // Delete the first node
+    {
+        *head = (*head)->next;
+    }
+    else if (previous->next == NULL)
+    {
+        return;
+    }
+    else
+    {
+        NODEPTR temp = previous->next->next;
+        previous->next = temp;
+    }
 }
